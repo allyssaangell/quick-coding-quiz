f546b40540dfe51112e648e3c2811eae5a64f7d0
allyssaangell <a.angell2@yahoo.com>
2022-01-22 19:19:52 -0600

    timer change

--- assets/script.js
+++ assets/script.js
@@ -5,13 +5,28 @@ var answerEl = document.getElementById("answer-buttons");
 var startButton = document.getElementById("start-btn");
 var indexQuestion = 0;
 var score = 0;
+var timer = document.querySelector("#timer");
+var timerSeconds = 60;
 
 startButton.addEventListener("click", startQuiz);
+timer.innerHTML = `Time Remaining: 00:${timerSeconds}`;
 
 function startQuiz() {
   startButton.classList.add("hide");
   questionContainerEl.classList.remove("hide");
   intro.classList.add("hide");
+
+  var countDown = setInterval(() => {
+    timerSeconds--;
+    timer.innerHTML = `Time Remaining: 00:${
+      timerSeconds < 10 ? "0" : ""
+    }${timerSeconds}`;
+    if (timerSeconds < 1) {
+      clearInterval(countDown);
+      timer.innerHTML = "Time's Up!";
+    }
+  }, 1000);
+
   displayQuestion();
   score = 0;
 }
@@ -53,7 +68,9 @@ var questions = [
 
 function displayQuestion() {
   if (indexQuestion >= questions.length) {
-    ending();
+    //  END OF QUIZ
+    document.getElementById("high-score-page").innerHTML =
+      "The quiz has ended! Your score: " + score;
   } else {
     var answerClick = document.getElementById("answer-buttons");
     var question = document.getElementById("question");
@@ -68,7 +85,6 @@ function displayQuestion() {
       listElement.textContent = questions[indexQuestion].answers[i];
       listElement.onclick = evaluateAndIncrement;
       answerClick.appendChild(listElement);
-
       i++;
     }
     question.textContent = questions[indexQuestion].question;
@@ -78,61 +94,33 @@ function displayQuestion() {
 function evaluateAndIncrement(event) {
   event.preventDefault();
   if (questions[indexQuestion].correctAnswer === event.target.innerHTML) {
-    score= score + 10;
+    score = score + 10;
     console.log("This is the score " + score);
+    document.getElementById("correct-wrong").innerHTML = "Correct!";
+    setTimeout(function () {
+      document.getElementById("correct-wrong").innerHTML = "";
+    }, 500);
   } else {
-      console.log("before time penalty =" + timerSeconds)
-      timerSeconds = timerSeconds - 10;
-      console.log("after time penalty =" + timerSeconds)
+    console.log("before time penalty =" + timerSeconds);
+    timerSeconds = timerSeconds - 10;
+    console.log("after time penalty =" + timerSeconds);
+    document.getElementById("correct-wrong").innerHTML = "Wrong!";
+    setTimeout(function () {
+      document.getElementById("correct-wrong").innerHTML = "";
+    }, 500);
   }
   indexQuestion++;
   displayQuestion();
 }
 
+if (timerSeconds > 0) {
+  clearInterval(countDown);
+  console.log("time remaining");
+} else {
+  //  current score entry?
+}
 
-
-var timer = document.querySelector("#timer");
-var timerSeconds = 60;
-
-timer.innerHTML = `Time Remaining: 00:${timerSeconds}`;
-
-var countDown = setInterval(() => {
-  timerSeconds--;
-  timer.innerHTML = `Time Remaining: 00:${timerSeconds < 10 ? "0" : ""}${timerSeconds}`;
-  if (timerSeconds < 1) {
-    clearInterval(countDown);
-    timer.innerHTML = "Time's Up!";
-  }
-}, 1000);
-
-//document.querySelector("#start-btn").addEventListener("click", startQuiz);
-//function for decrement timer to use the function
-
-// var getQuestion = function(){
-//     var questionSelector = document.querySelector("#question");
-//     var question = "question here?";
-//     var answers = ["one", "two", "three", "four"];
-
-//     questionSelector.textContent = question;
-
-//     for (var i = 0; i < answers.length; i++) {
-//         var answerSelector = document.createElement("button");
-//         answerSelector.textContent = answers[i];
-//         //append the buttons inside the for loop, add in 61 and 64
-//         answerSelector.className = "answer-buttons";
-
-//         answerSelector.appendChild(answers);
-
-//     }
-// }
-// document.querySelector("#start-btn").addEventListener("click", getQuestion, countDown);
-//new question should be a function
-
-// display correct or wrong verbiage
-
-//timer countdown
-
-//reduce time if wrong
+function highScoreEntry() {}
 
 //highscore entry
 
