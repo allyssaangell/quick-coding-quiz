074da9222965e4dad415913e171ae8ade3e995e2
allyssaangell <a.angell2@yahoo.com>
2022-01-23 19:11:26 -0600

    format document and readme

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # quick-coding-quiz
+
+This quiz shows examples of JavaScript fundamentals, both in quiz content as well as how the application is made.
+
+Here is a link to the deployed URL: https://allyssaangell.github.io/quick-coding-quiz/
+
+Here is a Screenshot of what you will find at the above URL:
--- assets/highscores.js
+++ assets/highscores.js
@@ -1,6 +1,7 @@
 var highScores = JSON.parse(localStorage.getItem("highScores")) || [];
 
-      highScores.forEach((element, index) => {
-        console.log(element.name + " - " + element.score);
-        document.getElementById("highScoresList").innerHTML += element.name + " - " + element.score + "<br>";
-      });
+highScores.forEach((element, index) => {
+  console.log(element.name + " - " + element.score);
+  document.getElementById("highScoresList").innerHTML +=
+    element.name + " - " + element.score + "<br>";
+});
